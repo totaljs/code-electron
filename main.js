b0307d1494ca0344c7acaf463205ec81d9496884
Peter Širka <petersirka@gmail.com>
2018-11-24 22:53:06 +0100

    Fixed `menu`.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const BrowserWindow = electron.BrowserWindow;
 const windowStateKeeper = require('electron-window-state');
 const path = require('path');
 const cache = {};
+const { Menu, MenuItem } = require('electron');
 
 let mainWindow;
 
@@ -34,10 +35,33 @@ function createWindow () {
 	});
 
 	mainWindow = window;
+	mainWindow.setMenuBarVisibility(false);
 	mainWindowState.manage(window);
 	window.loadFile('index.html');
+
+	var template = [{
+		label: 'Application',
+		submenu: [
+			{ label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+			{ type: 'separator' },
+			{ label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); }}
+		]}, {
+		label: 'Edit',
+		submenu: [
+			{ label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+			{ label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+			{ type: 'separator' },
+			{ label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+			{ label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+			{ label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+			{ label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+		]}
+	];
+
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 app.on('ready', createWindow);
 app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit());
 app.on('activate', () => mainWindow == null && createWindow());
+
