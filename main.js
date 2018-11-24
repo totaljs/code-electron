f6cd9c6a76b84b48839a2cbddad8cd968d9f988b
Peter Širka <petersirka@gmail.com>
2018-11-24 15:28:23 +0100

    Added CDN.

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ electron.ipcMain.on('getPath', function(e, arg) {
 
 function createWindow () {
 	var mainWindowState = windowStateKeeper({ defaultWidth: 1280, defaultHeight: 768 });
-	var window = new BrowserWindow({ icon: __dirname + '/icon.png', x: mainWindowState.x, y: mainWindowState.y, width: mainWindowState.width, height: mainWindowState.height, webPreferences: { nodeIntegration: true }, vibrancy: 'dark' });
+	var window = new BrowserWindow({ icon: __dirname + '/icon.png', x: mainWindowState.x, y: mainWindowState.y, width: mainWindowState.width, height: mainWindowState.height, webPreferences: { nodeIntegration: true }});
 	window.on('closed', function () {
 		if (mainWindow == this)
 			mainWindow = null;
@@ -32,6 +32,7 @@ function createWindow () {
 	window.on('focus', function () {
 		mainWindow = this;
 	});
+
 	mainWindow = window;
 	mainWindowState.manage(window);
 	window.loadFile('index.html');
@@ -42,9 +43,9 @@ app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit());
 app.on('activate', () => mainWindow == null && createWindow());
 
 app.on('ready', () => {
-	electron.globalShortcut.register('CommandOrControl+N', createWindow);
-	electron.globalShortcut.register('Command+R', function() {});
-	electron.globalShortcut.register('Command+W', function() {
-		mainWindow.webContents.send('tabs.close');
-	});
+//	electron.globalShortcut.register('CommandOrControl+N', createWindow);
+//	electron.globalShortcut.register('Command+R', function() {});
+//	electron.globalShortcut.register('Command+W', function() {
+//		mainWindow.webContents.send('tabs.close');
+//	});
 });
