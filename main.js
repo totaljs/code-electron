1516de273e714a6a504baee793fb95a13ae3f254
Peter Širka <petersirka@gmail.com>
2018-12-01 14:57:36 +0100

    Improved UI.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const electron = require('electron');
+const appConfig = require('electron-settings');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const windowStateKeeper = require('electron-window-state');
+var open = require('open');
 const path = require('path');
 const cache = {};
-const { Menu, MenuItem } = require('electron');
+const { Menu } = require('electron');
 
 let mainWindow;
 
@@ -24,19 +25,38 @@ electron.ipcMain.on('getPath', function(e, arg) {
 });
 
 function createWindow () {
-	var mainWindowState = windowStateKeeper({ defaultWidth: 1280, defaultHeight: 768 });
-	var window = new BrowserWindow({ autoHideMenuBar: true, icon: __dirname + '/icon.png', x: mainWindowState.x, y: mainWindowState.y, width: mainWindowState.width, height: mainWindowState.height, webPreferences: { nodeIntegration: true }});
+	var mainWindowState = windowStateKeeper('main');
+	var window = new BrowserWindow({ autoHideMenuBar: true, icon: __dirname + '/icon.png', x: mainWindowState.x, y: mainWindowState.y, width: mainWindowState.width, height: mainWindowState.height, webPreferences: { nodeIntegration: true, nativeWindowOpen: true }});
+
+	mainWindowState.track(window);
+
 	window.on('closed', function () {
 		if (mainWindow == this)
 			mainWindow = null;
 	});
+
+	window.webContents.on('new-window', function(e, url, frameName, disposition, options) {
+		e.preventDefault();
+		if (url.indexOf('/api/download/') !== -1 && url.indexOf('?path') !== -1) {
+			Object.assign(options, { width: 1280, height: 768, modal: false });
+			var mws = windowStateKeeper('preview');
+			options.autoHideMenuBar = true;
+			options.x = mws.x;
+			options.y = mws.y;
+			options.width = mws.width;
+			options.height = mws.height;
+			e.newGuest = new BrowserWindow(options);
+			mws.track(e.newGuest);
+		} else
+			open(url);
+	});
+
 	window.on('focus', function () {
 		mainWindow = this;
 	});
 
 	mainWindow = window;
 	mainWindow.setMenuBarVisibility(false);
-	mainWindowState.manage(window);
 	window.loadFile('index.html');
 
 	var template = [{
@@ -62,6 +82,39 @@ function createWindow () {
 	];
 
 	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
+function windowStateKeeper(windowName) {
+
+	let window, windowState;
+
+	function setBounds() {
+		// Restore from appConfig
+		if (appConfig.has(`windowState.${windowName}`)) {
+			windowState = appConfig.get(`windowState.${windowName}`);
+			return;
+		}
+		// Default
+		windowState = { x: undefined, y: undefined, width: 1000, height: 800 };
+	}
+
+	function saveState() {
+		if (!windowState.isMaximized) {
+			windowState = window.getBounds();
+		}
+		windowState.isMaximized = window.isMaximized();
+		appConfig.set(`windowState.${windowName}`, windowState);
+	}
+
+	function track(win) {
+		window = win;
+		['resize', 'move', 'close'].forEach(event => {
+			win.on(event, saveState);
+		});
+	}
+
+	setBounds();
+	return({ x: windowState.x, y: windowState.y, width: windowState.width, height: windowState.height, isMaximized: windowState.isMaximized, track });
 }
 
 app.on('ready', createWindow);
